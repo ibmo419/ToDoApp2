9bd33256ee95274abdc19b84bc0905ac2d03a437
Mohamed <hadjamor.medali@gmail.com>
2021-05-14 06:39:49 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoForm from './Component/TodoForm';
+import TodoList from './Component/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/TodoForm.js
@@ -0,0 +1,37 @@
+import React,{useState,useRef,useEffect} from 'react'
+
+function TodoForm(props) {
+    const [input, setInput] = useState('')
+    
+    const inputRef=useRef(null)
+    useEffect(()=>{
+        inputRef.current.focus()
+    })
+
+    const handleChange=e=>{
+        setInput(e.target.value);
+    };
+
+    const handelSubmit=e=>{
+        e.preventDefault();
+        props.onSubmit({
+            id:Math.floor(Math.random() * 1000),
+            text:input
+        });
+        setInput('');
+    };
+    return (
+        <form className='todo-form' onSubmit={handelSubmit}>
+        <input type='text' 
+        placeholder='Add to do' 
+        name='text' value={input} 
+        className='todo-input' 
+        onChange={handleChange}
+        ref={inputRef}
+        />
+        <button className='todo-button'>Add todo</button>
+        </form>
+    )
+}
+
+export default TodoForm
